1fbe5348b807fc1ce1b91e7c188b51e05237151f
Saulo Martins <saulo.martins@totvs.com.br>
2021-01-06 11:38:28 -0300

    teste webpush

---
+++ webpush/sw.js
@@ -0,0 +1,33 @@
+// Copyright 2015 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(clients.claim());
+});
+
+self.addEventListener('push', function(event) {	
+	var data = event.data.json();
+
+	var title = data.title;
+	var options = data.options;
+
+	event.waitUntil(
+		self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+	const clickedNotification = event.notification;
+	clickedNotification.close();
+	if (!event.action) {
+		if (typeof event.notification.data["url"] == "string") {
+			event.waitUntil(
+				clients.openWindow(event.notification.data.url)
+			)
+		}
+	}
+  });
